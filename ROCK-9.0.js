71bcaf20bbbdd2d852ac90c498eda0a93df720b3
James Rock <hello@jamesrock.me>
2018-06-01 08:40:31 +0100

    updates

--- ROCK-9.0.js
+++ ROCK-9.0.js
@@ -713,11 +713,9 @@
 })();
 (function() {
 
-	ROCK.HTTP = {};
-
-	HTTPRequest = ROCK.Object.extend({
+	var HTTPRequest = ROCK.Object.extend({
 		constructor: function HTTPRequest(url, callback) {
-		
+
 			this.url = url;
 			this.request = new XMLHttpRequest();
 
@@ -726,15 +724,15 @@
 					callback(JSON.parse(this.responseText));
 				};
 			};
-			
+
 		},
 		paramsToJSON: function() {
-			
+
 			return JSON.stringify(this.params);
 
 		},
 		paramCheck: function() {
-			
+
 			for(var param in this.params) {
 				return true;
 			};
@@ -742,30 +740,30 @@
 
 		},
 		setParam: function(key, value) {
-			
+
 			this.params[key] = value;
 			return this;
 
 		},
 		getParam: function(key) {
-			
+
 			return this.params[key];
 
 		},
 		removeParam: function(key, value) {
-			
+
 			delete this.params[key];
 			return this;
 
 		},
 		setHeader: function(key, value) {
-			
+
 			this.headers[key] = value;
 			return this;
 
 		},
 		addHeaders: function() {
-			
+
 			for(var header in this.headers) {
 				this.request.setRequestHeader(header, this.headers[header]);
 			};
@@ -773,7 +771,7 @@
 
 		},
 		abort: function() {
-			
+
 			this.request.abort();
 
 		},
@@ -784,40 +782,40 @@
 		params: {},
 		dataType: "json"
 	});
-	
-	ROCK.HTTP.GET = HTTPRequest.extend({
-		send: function() {
 
-			var
-			url = this.url;
+	ROCK.HTTP = {
+		GET: HTTPRequest.extend({
+			send: function() {
 
-			if(this.paramCheck()) {
-				url += ("?" + this.paramsToJSON());
-			};
-			
-			this.request.open(this.type, url, true);
-			this.addHeaders();
-			this.request.send();
-			
-		},
-		type: "GET"
-	});
-	
-	ROCK.HTTP.POST = HTTPRequest.extend({
-		send: function() {
-			
-			this.request.open(this.type, this.url, true);
-			this.addHeaders();
-			this.request.send(this.paramCheck()?this.paramsToJSON():null);
-		
-		},
-		type: "POST"
-	});
+				var
+				url = this.url;
 
-	ROCK.HTTP.makeURL = function(url, params) {
+				if(this.paramCheck()) {
+					url += ("?" + this.paramsToJSON());
+				};
 
-		
+				this.request.open(this.type, url, true);
+				this.addHeaders();
+				this.request.send();
 
+			},
+			type: "GET"
+		}),
+		POST: HTTPRequest.extend({
+			send: function() {
+
+				this.request.open(this.type, this.url, true);
+				this.addHeaders();
+				this.request.send(this.paramCheck()?this.paramsToJSON():null);
+
+			},
+			type: "POST"
+		}),
+		makeURL: function(url, params) {
+
+
+
+		}
 	};
 
 })();
